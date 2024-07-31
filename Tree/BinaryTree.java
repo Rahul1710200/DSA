0c60ad6d3d02b638a026f695e58de82f462ae5e3
Rahul Garg <rahulgarg17102001@gmail.com>
2024-07-31 19:11:07 +0530

    js added

--- Tree/BinaryTree.java
+++ Tree/BinaryTree.java
@@ -5,7 +5,7 @@
  */
 public class BinaryTree {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hellogit fetch");
     }
 
     
--- Tree/BinaryTree2.java
+++ Tree/BinaryTree2.java
@@ -1,5 +1,8 @@
 package DSA.Tree;
 
 public class BinaryTree2 {
+    public static void main(String[] args) {
+        
+    }
     
 }
